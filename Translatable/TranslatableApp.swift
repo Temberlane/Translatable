855850371f70fe69efd7febc9c268c0664d28dbd
Temberlane <60454907+Temberlane@users.noreply.github.com>
2025-01-09 23:49:08 -0500

    Initial Commit

---
+++ Translatable/TranslatableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatableApp.swift
+//  Translatable
+//
+//  Created by Thomas Li on 2025-01-09.
+//
+
+import SwiftUI
+
+@main
+struct TranslatableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TranslatableTests/TranslatableTests.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatableTests.swift
+//  TranslatableTests
+//
+//  Created by Thomas Li on 2025-01-09.
+//
+
+import Testing
+@testable import Translatable
+
+struct TranslatableTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
